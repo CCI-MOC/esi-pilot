af4c2dcb8a2e153b1a8ac9c7e28a25482fa613a2
Danni Shi <sdanni@redhat.com>
2022-03-22 09:59:43 -0400

    update necessary files

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-openstack overcloud deploy --stack overcloud  --working-dir ~/overcloud-deploy/overcloud \
-	  --templates ~/tripleo-heat-templates \
-	  -e ~/ironic-overcloud.yaml \
-	  -n ~/custom_network_data.yaml \
-	  --baremetal-deployment ~/overcloud_baremetal_deploy.yaml \
-	  -r ~/roles_data.yaml \
+openstack overcloud deploy --stack overcloud \
+	  --templates /usr/share/openstack-tripleo-heat-templates \
+	  -n custom_network_data.yaml \
+	  --baremetal-deployment overcloud_baremetal_deploy.yaml \
+	  -r roles_data.yaml \
 	  --deployed-server \
-	  -e ~/containers-prepare-parameter.yaml \
+	  -e containers-prepare-parameter.yaml \
 	  -e /usr/share/openstack-tripleo-heat-templates/environments/memcached-use-ips.yaml \
-	  -e ~/esi-custom.yaml \
+      -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ml2-ansible.yaml \
+	  -e ml2-ansible-hosts.yaml \
+	  -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+	  -e esi-custom.yaml
