be75940636d518deaea42e29f9145133cbba06be
Tzu-Mainn Chen <tzumainn@redhat.com>
2022-11-02 13:10:02 -0400

    update external network parameters

--- openstack_commands/network.sh
+++ openstack_commands/network.sh
@@ -3,39 +3,43 @@
 # provisioning network
 openstack network create --provider-network-type vlan --provider-segment 623 --provider-physical-network datacentre --share provisioning
 openstack subnet create \
-    --network provisioning \
-    --subnet-range 192.168.11.0/24  \
-    --ip-version 4 \
-    --gateway 192.168.11.254 \
-    --allocation-pool start=192.168.11.130,end=192.168.11.149 \
-    --dns-nameserver 8.8.8.8 \
-    --dhcp subnet-provisioning
+      --network provisioning \
+      --subnet-range 192.168.11.0/24  \
+      --ip-version 4 \
+      --gateway 192.168.11.254 \
+      --allocation-pool start=192.168.11.100,end=192.168.11.149 \
+      --dns-nameserver 8.8.8.8 \
+      --dhcp subnet-provisioning
 
 # ctlplane network
 openstack network create --provider-network-type flat --provider-physical-network  datacentre ctlplane
 openstack subnet create --network ctlplane --subnet-range 192.168.24.0/24  --ip-version 4 --gateway 192.168.24.254 --no-dhcp ctlplane-subnet
-openstack router create provisionrouter
-openstack router add subnet provisionrouter subnet-provisioning
-openstack router add subnet provisionrouter ctlplane-subnet
 
 # storage network
 openstack network create --provider-network-type vlan --provider-segment 213 --provider-physical-network datacentre  --share storage
 openstack subnet create \
-    --network storage \
-    --subnet-range 10.21.0.0/22  \
-    --ip-version 4 \
-    --gateway 10.21.0.1 \
-    --allocation-pool start=10.21.0.140,end=10.21.0.159 \
-    --dns-nameserver 8.8.8.8 \
-    --dhcp subnet-storage
+          --network storage \
+          --subnet-range 10.21.0.0/22  \
+          --ip-version 4 \
+          --gateway 10.21.0.1 \
+          --allocation-pool start=10.21.0.140,end=10.21.0.159 \
+          --dns-nameserver 8.8.8.8 \
+          --dhcp subnet-storage
 
-#external network
+# external network
 openstack network create --provider-network-type vlan --provider-segment 3801 --provider-physical-network datacentre  --external --share external
 openstack subnet create \
-    --network external \
-    --subnet-range 128.31.20.0/24  \
-    --ip-version 4 \
-    --gateway 128.31.20.1 \
-    --allocation-pool start=128.31.20.21,end=128.31.20.240 \
-    --dns-nameserver 8.8.8.8 \
-    --dhcp subnet-external
+          --network external \
+          --subnet-range 128.31.20.0/22  \
+          --ip-version 4 \
+          --gateway 128.31.20.1 \
+          --allocation-pool start=128.31.20.31,end=128.31.20.240 \
+          --dns-nameserver 8.8.8.8 \
+          --dhcp subnet-external
+
+# provisioning router
+openstack router create provisionrouter
+openstack router add subnet provisionrouter subnet-provisioning
+openstack router add subnet provisionrouter ctlplane-subnet
+openstack router add subnet provisionrouter subnet-storage
+openstack router set --external-gateway external provisionrouter
