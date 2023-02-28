b57e6a23d886b5e5538f44e5c8abbc4ce75bb560
Tzu-Mainn Chen <tzumainn@redhat.com>
2023-02-28 16:17:57 -0500

    Updated parameters for a minor update and added documentation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # esi-pilot
 
 ## Get access to overcloud:
+
 ### Admin user:
 1. Store the `admin_rc` file on your machine and `source admin_rc`
 2. Create the non-admin project/user as the examples in `create_project.sh` in this repo
---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+openstack overcloud update prepare --stack overcloud \
+		--validation-errors-nonfatal \
+		--templates /usr/share/openstack-tripleo-heat-templates \
+		-n custom_network_data.yaml \
+		--vip-file custom_vip_data.yaml \
+		--baremetal-deployment overcloud_baremetal_deploy.yaml \
+		-r roles_data.yaml \
+		--deployed-server \
+		-e containers-prepare-parameter.yaml \
+		-e /usr/share/openstack-tripleo-heat-templates/environments/memcached-use-ips.yaml \
+		-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ml2-ansible.yaml \
+		-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+		-e enable-tls.yaml \
+		-e tls-endpoints-public-dns.yaml \
+		-e cloudname.yaml \
+		-e esi-custom.yaml
+
