08c040edb61488d4026d26ab5060562779c09b49
Hakan Saplakoglu <hakansaplakog@gmail.com>
2024-10-11 12:20:11 -0400

    removed share argument from 2176 routed

--- openstack_commands/owned_networks.sh
+++ openstack_commands/owned_networks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # NERC 2176 routed network
-openstack network create --provider-network-type vlan --provider-segment 2076 --provider-physical-network datacentre --project nerc-admins --share nerc-infra-routed
+openstack network create --provider-network-type vlan --provider-segment 2076 --provider-physical-network datacentre --project nerc-admins nerc-infra-routed
 openstack subnet create \
           --network nerc-infra-routed \
           --subnet-range 10.85.0.0/22  \
