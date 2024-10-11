5d1b004e1b2eb267bc0f8d4a227181cb928ce52e
Hakan Saplakoglu <hakansaplakog@gmail.com>
2024-10-11 10:23:30 -0400

    added nerc infra routed network

--- openstack_commands/network.sh
+++ openstack_commands/network.sh
@@ -49,6 +49,16 @@ openstack subnet create \
           --host-route destination=140.247.236.0/25,gateway=10.0.120.1 \
           --dhcp subnet-nese-storage
 
+# NERC 2176 routed network
+openstack network create --provider-network-type vlan --provider-segment 2076 --provider-physical-network datacentre --project nerc-admins --share nerc-infra-routed
+openstack subnet create \
+          --network nerc-infra-routed \
+          --subnet-range 10.85.0.0/22  \
+          --ip-version 4 \
+          --allocation-pool start=10.85.1.0,end=10.85.3.254 \
+          --host-route destination=10.30.9.0/24,gateway=10.85.0.1 \
+          --dhcp subnet-nerc-infra-routed
+
 # provisioning router
 openstack router create provisionrouter
 openstack router add subnet provisionrouter subnet-provisioning
